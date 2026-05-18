8c5b47bd0fe5247d9b4654f8250d2a43c619219e
enwolf <35503014+enwolf@users.noreply.github.com>
2026-05-18 19:52:52 -0300

    Updated User class, added user class properties, installed faker to generate fake data, used faker to initialized my class properties in a constructor

--- maps/src/User.ts
+++ maps/src/User.ts
@@ -1,5 +1,24 @@
-class user 
-{
+import faker from 'faker';
 
+class user{
+    name: string;
+
+    location: {
+        latitude: number;
+        longitude: number;
+    };
+
+    constructor() {
+
+        this.name = faker.name.firstName();
+        this.location = {
+
+            latitude: parseFloat(faker.address.latitude()),
+            longitude: parseFloat(faker.address.longitude()) 
+
+        };
+
+
+    }
 
 }
