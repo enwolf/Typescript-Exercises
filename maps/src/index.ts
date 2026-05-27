39120341e49f3e0100a586cc12afaa93bcc1ce1a
enwolf <35503014+enwolf@users.noreply.github.com>
2026-05-27 19:56:56 -0300

    Created New File CustomMap.ts moved logic from index.ts into this class and gave the constructor of type string for the html element we wanted to targe. Then in  index.ts Icreated a call to new Map vis constructor argument thus moving the logic into its own file, .gitignore upates as well as settings to make runing server more convenient

--- maps/src/index.ts
+++ maps/src/index.ts
@@ -8,13 +8,9 @@ console.log(user);
 const company = new Company();
 console.log(company);*/
 
-new google.maps.Map(document.getElementById("map") as HTMLElement, {
-    zoom: 1,
-    center: { 
-        lat: 0, 
-        lng: 0 
-    }
-});     
+import { CustomeMap } from "./CustomeMap";
+
+new CustomeMap("map");
 
 
 
