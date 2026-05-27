f74d79eeb2c30b825939df80a05fdff1e756ec71
enwolf <35503014+enwolf@users.noreply.github.com>
2026-05-27 17:23:54 -0300

    Updated index.html added div with id map and style of hight 100% Added Script for google maps src. As per instuctions added referance of types google.maps commented out orginal code for now, added code to create a  google maps object as HTMLElement with zoom of 1 and centered at lat 0 and lng 0 the API key is here as well.

--- maps/src/index.ts
+++ maps/src/index.ts
@@ -1,8 +1,26 @@
-import { User } from "./User";
+/// <reference types="@types/google.maps" />
+/*import { User } from "./User";
 import { Company } from "./Company";
 
 const user = new User();
 console.log(user);
 
 const company = new Company();
-console.log(company);
+console.log(company);*/
+
+new google.maps.Map(document.getElementById("map") as HTMLElement, {
+    zoom: 1,
+    center: { 
+        lat: 0, 
+        lng: 0 
+    }
+});     
+
+
+
+
+"AIzaSyAXwRNFksnxRXxiGVTLeQGaWOyJ7qJzH8M -old" 
+
+
+
+"AIzaSyDeKJ9pWkXF5wVTfKCkL_8SmomMqd_2fMs -use me!"
